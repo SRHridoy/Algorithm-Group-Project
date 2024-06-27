6a7090c0aa3354c12a1fed1b84cdea4d49ad1f94
SRHridoy <sohanurrahmanhridoy007@gmail.com>
2024-06-27 16:39:04 +0600

    Completed Bubble Sort Activity and all Functionality...

--- app/src/main/java/com/elitcoder/algoexpress/BubbleSortActivity.java
+++ app/src/main/java/com/elitcoder/algoexpress/BubbleSortActivity.java
@@ -1,6 +1,9 @@
 package com.elitcoder.algoexpress;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +11,36 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class BubbleSortActivity extends AppCompatActivity {
+import com.elitcoder.algoexpress.databinding.ActivityBubbleSortBinding;
 
+public class BubbleSortActivity extends AppCompatActivity {
+    ActivityBubbleSortBinding bubbleSortBinding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_bubble_sort);
+        //View Binding :
+        bubbleSortBinding = ActivityBubbleSortBinding.inflate(getLayoutInflater());
+        View view = bubbleSortBinding.getRoot();
+        setContentView(view);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        //visulaizeBubbleSort :
+        goToVisualizeBubbleSort();
+    }
+
+    private void goToVisualizeBubbleSort() {
+        bubbleSortBinding.btnBubbleVisualize.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent goToVisualizeBubble = new Intent(BubbleSortActivity.this,VisualizeBubbleActivity.class);
+                startActivity(goToVisualizeBubble);
+            }
         });
     }
 }
---
+++ app/src/main/java/com/elitcoder/algoexpress/VisualizeBubbleActivity.java
@@ -0,0 +1,93 @@
+package com.elitcoder.algoexpress;
+
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import com.elitcoder.algoexpress.databinding.ActivityVisualizeBubbleBinding;
+
+public class VisualizeBubbleActivity extends AppCompatActivity {
+    ActivityVisualizeBubbleBinding visualizeBubbleBinding;
+    String result;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        //View Binding :
+        visualizeBubbleBinding = ActivityVisualizeBubbleBinding.inflate(getLayoutInflater());
+        View view = visualizeBubbleBinding.getRoot();
+        setContentView(view);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        //Visualize Bubbble:
+        callVisulalizer();
+    }
+
+    private void callVisulalizer() {
+        visualizeBubbleBinding.btnBubbleVisualize.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                visualizeBubble();
+            }
+        });
+    }
+
+    private void visualizeBubble() {
+        String noOfElementsS = visualizeBubbleBinding.edtNoOfElements.getText().toString();
+        String elements = visualizeBubbleBinding.edtElements.getText().toString();
+
+        int noOfElements = Integer.parseInt(noOfElementsS);
+
+        String[] elementsArray = elements.split(" ");
+        int[] intArray = new int[elementsArray.length];
+
+        // Convert each element to an integer
+        for (int i = 0; i < elementsArray.length; i++) {
+            try {
+                intArray[i] = Integer.parseInt(elementsArray[i]);
+            } catch (NumberFormatException e) {
+                e.printStackTrace();
+            }
+        }
+        //Bubble Sort:\
+        bubbleSort(noOfElements,intArray);
+
+    }
+
+    void bubbleSort(int n, int[] intArray){
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < n-1; i++){
+            sb.append("Pass ").append(i+1).append(" : \n");
+            for(int j = 0; j<n-i-1;j++){
+                if(intArray[j]>intArray[j+1]){
+                    int temp;
+                    temp = intArray[j];
+                    intArray[j] = intArray[j+1];
+                    intArray[j+1] = temp;
+                }
+                for(int k = 0; k<n;k++){
+                    sb.append(intArray[k]).append(" ");
+                }sb.append("\n");
+            }sb.append("\n\n");
+        }
+
+        // Update UI with all appended steps
+        result = sb.toString();
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                visualizeBubbleBinding.txtBubbleResult.setVisibility(View.VISIBLE);
+                visualizeBubbleBinding.txtBubbleResult.setText(result);
+            }
+        });
+    }
+}
